4570ece00ecd7685de41c5e3a9b61a8581144229
Dhruva Kumar <dhr7va@gmail.com>
2024-02-25 20:41:56 +0530

    routeparam

--- index.jsx
+++ index.jsx
@@ -4,9 +4,9 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
 
 import "./server"
-
 
 function App() {
   return (
@@ -22,6 +22,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,4 +1,6 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
 
 /**
  * {
@@ -22,12 +24,14 @@ export default function Vans() {
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
     ))
 

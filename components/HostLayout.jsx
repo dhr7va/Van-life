b92e21c4d1349d4dfa3cdf90cd46806b5fd50c12
Dhruva Kumar <dhr7va@gmail.com>
2024-02-26 00:16:40 +0530

    added navLink

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,13 +1,52 @@
 import React from "react"
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 
 export default function HostLayout() {
+    /**
+     * Challenge - part 2:
+     * Make the host navbar indicate the currently-active route.
+     * 
+     * Use the following CSS rules:
+     *      font-weight: bold;
+     *      text-decoration: underline;
+     *      color: #161616;
+     * 
+     * I'd recommend using an inline style this time.
+     * 
+     * NOTE: There will be a small bug that we'll fix 
+     * after you do the challenge.
+     */
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
     return (
         <>
             <nav className="host-nav">
-                <Link to="/host">Dashboard</Link>
-                <Link to="/host/income">Income</Link>
-                <Link to="/host/reviews">Reviews</Link>
+                <NavLink
+                    to="/host"
+                    end
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Dashboard
+                </NavLink>
+
+                <NavLink
+                    to="/host/income"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Income
+                </NavLink>
+
+                <NavLink
+                    to="/host/reviews"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Reviews
+                </NavLink>
+
             </nav>
             <Outlet />
         </>

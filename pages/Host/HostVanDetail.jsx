685613180edf57cf5385a35a9888d5d0c7ee7166
Dhruva Kumar <dhr7va@gmail.com>
2024-02-26 12:30:01 +0530

    added detailing using use outlet context

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -67,7 +67,7 @@ export default function HostVanDetail() {
 
                 </nav>
 
-                <Outlet />
+                <Outlet context={{ currentVan }} />
             </div>
         </section>
     )

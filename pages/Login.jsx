4b31905c6ffdc821f6a9ac9b020d99a184b8d2f2
Dhruva Kumar <dhr7va@gmail.com>
2024-02-26 23:48:47 +0530

    Updating dashboard

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -9,6 +9,7 @@ export default function Login() {
 
     const location = useLocation()
     const navigate = useNavigate()
+    const from = location.state?.from || "/host";
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -17,7 +18,7 @@ export default function Login() {
             .then(data => {
                 setError(null)
                 localStorage.setItem("loggedin", true)
-                navigate("/host", { replace: true })
+                navigate(from, { replace: true })
             })
             .catch(err => {
                 setError(err)
